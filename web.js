1dd146dfa50d7427ff35a293403d8818d095ee94
Radek C <rcirmira@gmail.com>
2013-07-15 00:59:10 +0000

    define require for fs

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
 var buffer = fs.readFileSync('/index.html');
 
 app.get('/', function(request, response) {
